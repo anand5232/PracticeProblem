2fbf70aa2a5c50652a96a2cecee30f1a2a23a366
anand5232 <jhariyaanand000@gmail.com>
2023-05-08 20:45:04 +0530

    java code

---
+++ src/DefaultValue.java
@@ -0,0 +1,19 @@
+public class DefaultValue {
+    static int  val1;
+    static float  val2;
+    static double   val3;
+    static long  val4;
+    static  boolean  val5;
+    static char val6;
+
+    public static void main(String[] args) {
+        System.out.println("Default Values of data types ....");
+        System.out.println("int = "+val1);
+        System.out.println("Float = "+val2);
+        System.out.println("Double = "+val3);
+        System.out.println("Long = "+val4);
+        System.out.println("Boolean = "+val5);
+        System.out.println("Char = "+val6);
+
+    }
+}
